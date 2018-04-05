16d5ff5a5e458433cb2280bc4a81bbeb5a44a17e
maciejwelz <37769069+maciejwelz@users.noreply.github.com>
2018-04-05 20:22:19 +0200

    Delete funkcje.h

--- funkcje.h
+++
@@ -1,9 +0,0 @@
-int AreaOfTriangle(double a, double h);
-
-int AreaOfTrapezium(double a, double b, double h);
-
-int SumOfAngles(int a);
-
-int hypotenuse(double a, double b);
-
-int GCD(int a, int b);
