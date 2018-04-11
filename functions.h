91d892864e71c619ac8b9800b1f84f1943ce5832
maciejwelz <37769069+maciejwelz@users.noreply.github.com>
2018-04-11 12:54:12 +0200

    Update functions.h

--- functions.h
+++ functions.h
@@ -1,3 +1,7 @@
+#ifndef FUNCTIONS_H_INCLUDED
+#define FUNCTIONS_H_INCLUDED
+
+
 double AreaOfTriangle(double a, double h);
 
 double AreaOfTrapezium(double a, double b, double h);
@@ -7,3 +11,5 @@ int SumOfAngles(int a);
 double hypotenuse(double a, double b);
 
 int GCD(int a, int b);
+
+#endif // FUNCTIONS_H_INCLUDED
